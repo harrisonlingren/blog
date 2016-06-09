8ef5e11d1f8a25651a87b8a932a83cd0e181b698
Harrison Lingren <harrisonlingren@gmail.com>
2016-06-09 13:18:37 -0400

    update db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609131006) do
+ActiveRecord::Schema.define(version: 20160525220209) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
