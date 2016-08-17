76fc3f725c029fc9d05f6526efe236c91f2f9373
Lingren, Harrison <harrison.lingren@nextgearcapital.com>
2016-08-17 13:13:03 -0400

    re-add db schema

---
+++ db/schema.rb
@@ -0,0 +1,20 @@
+ActiveRecord::Schema.define(version: 20160609131006) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user"
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+end
