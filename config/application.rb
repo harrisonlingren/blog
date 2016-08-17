d23ea6cd70ae4a6c19d8a16b536275519c0e87f7
Lingren, Harrison <harrison.lingren@nextgearcapital.com>
2016-08-17 11:46:45 -0400

    define markdown renderers

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,8 @@ module Blog
     config.exceptions_app = self.routes
 
     # Initializes a Markdown parser
-    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+    renderer =
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, highlighted: true, strikethrough: true, superscript: true, underline: true, fenced_code_blocks: true, escape_html: true)
 
   end
 end
