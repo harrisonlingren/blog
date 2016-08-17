f3ecba1d76d06fbf79ce8d5c751f899e0c6131cb
Lingren, Harrison <harrison.lingren@nextgearcapital.com>
2016-08-17 10:40:51 -0400

    fix missed 'article' reference, rebuild db schema and change name

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
   def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
   end
 
   private
